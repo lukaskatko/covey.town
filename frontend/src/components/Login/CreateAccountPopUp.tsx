3b6b8b4d16c794d038c6ee736375af5687aa3d1a
Pablo Forero <ps.gomezforero@gmail.com>
2021-04-15 21:04:20 -0400

    Avatar selection updates but not on login

--- frontend/src/components/Login/CreateAccountPopUp.tsx
+++ frontend/src/components/Login/CreateAccountPopUp.tsx
@@ -14,7 +14,7 @@ import {
   useToast,
 } from '@chakra-ui/react';
 import React, { useState } from 'react';
-import { addUser } from '../../classes/api';
+import { addUser, updateUser } from '../../classes/api';
 
 const CreateAccountPopUp: React.FunctionComponent = () => {
   const { isOpen, onOpen, onClose } = useDisclosure();
